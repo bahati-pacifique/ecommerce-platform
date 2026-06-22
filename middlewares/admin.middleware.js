6919c2da40bbd28c58c32f632c924bc4e7c1b036
genistack <genistack@outlook.com>
2026-06-22 13:55:53 +0200

    Implemented: Admin Host test logs

--- middlewares/admin.middleware.js
+++ middlewares/admin.middleware.js
@@ -3,6 +3,8 @@ module.exports = (req, res, next) => {
     const isProduction =
         process.env.NODE_ENV === 'production';
 
+        console.log(isProduction, process.env.ADMIN_HOST, req.hostname);
+
     if (!isProduction) {
         return next();
     }
