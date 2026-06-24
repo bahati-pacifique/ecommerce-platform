56abc96f6b614e7f4a6d665cf5ed7eda57db98cc
genistack <genistack@outlook.com>
2026-06-24 15:47:42 +0200

    Added: Pulling meta resources from cloudinary bucket

--- apps/admin/controller/admin.controller.js
+++ apps/admin/controller/admin.controller.js
@@ -2,13 +2,53 @@ const mockService = require('../../../src/services/DBMockService');
 
 async function home(req, res) {
     try {
+        downloadImage()
+            .then((filePath) => {
+                console.log('Downloaded to:', filePath);
+            })
+            .catch(console.error);
         await mockService.testConnection();
     } catch (error) {
         console.log(error);
     }
     //Middleware check admin session
-    return res.render('index-admin', {message: ''});
+    return res.render('index-admin', { message: '' });
 }
+
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+async function downloadImage() {
+    const url = 'https://res.cloudinary.com/dfv97pfcq/image/upload/v1717404221/cococe/cococe/p6nxkogue1wlimtlaohz.png';
+
+    const folder = '/var/www/cococe-storage/meta';
+    const fileName = 'image.png'; // or generate unique name
+    const filePath = path.join(folder, fileName);
+
+    // ensure folder exists
+    fs.mkdirSync(folder, { recursive: true });
+
+    const response = await axios({
+        url,
+        method: 'GET',
+        responseType: 'stream'
+    });
+
+    const writer = fs.createWriteStream(filePath);
+
+    response.data.pipe(writer);
+
+    return new Promise((resolve, reject) => {
+        writer.on('finish', () => {
+            resolve(filePath);
+        });
+
+        writer.on('error', reject);
+    });
+}
+
+// usage
 
 module.exports = {
     home
